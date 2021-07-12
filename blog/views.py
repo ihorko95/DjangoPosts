de412414804504e4b9ecd5728e3e82d95e9fb212
Ihor Kolisnyk <ihorko95@gmail.com>
2021-07-12 11:58:58 +0300

    refactoring

--- blog/views.py
+++ blog/views.py
@@ -1,21 +1,31 @@
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404
 #from django.http import HttpResponse
 from .models import Post, Tag
 from django.views.generic import View
+
+from .utils import *
+
 def hello(request):
     p= Post.objects.all()
     return render(request, 'blog/index.html', context={'posts': p})
 
 
-class PostDetail(View):
-    def get(self, request, slug):
-        p = Post.objects.get(slug__iexact=slug)
-        return render(request, 'blog/post_detail.html', context={'post': p})
+class PostDetail(ObjectDetail,View):
+    model = Post
+    template = 'blog/post_detail.html'
+    # def get(self, request, slug):
+    #     # p = Post.objects.get(slug__iexact=slug)
+    #     p = get_object_or_404(Post, slug__iexact=slug)
+    #     return render(request, 'blog/post_detail.html', context={'post': p})
 def tags_list(request):
     tags = Tag.objects.all()
     return render(request, 'blog/tags_list.html', context ={'tags':tags})
 
-class TagDetail(View):
-    def get(self, request,slug):
-        tag = Tag.objects.get(slug__iexact=slug)
-        return render(request, 'blog/tag_detail.html', context={'tag': tag})
+class TagDetail(ObjectDetail,View):
+    model = Tag
+    template = 'blog/tag_detail.html'
+    # def get(self, request,slug):
+    #     # tag = Tag.objects.get(slug__iexact=slug)
+    #     tag = get_object_or_404(Tag,slug__iexact=slug)
+    #     return render(request, 'blog/tag_detail.html', context={'tag': tag})
