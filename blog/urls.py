2f58f9ce35c3ba7a19753b798477f2988332e7de
Ihor Kolisnyk <ihorko95@gmail.com>
2021-07-12 11:36:25 +0300

    refactoring

--- blog/urls.py
+++ blog/urls.py
@@ -3,8 +3,8 @@ from .views import *
 
 urlpatterns = [
     path('', hello, name='post_list_url'),
-    path('post/<str:slug>/', post_detail, name='post_detail_url'),
+    path('post/<str:slug>/', PostDetail.as_view(), name='post_detail_url'),
     path('tags/',tags_list,name='tags_list_url'),
-    path('tag/<str:slug>/', tag_detail, name='tag_detail_url')
+    path('tag/<str:slug>/', TagDetail.as_view(), name='tag_detail_url')
 
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,18 +1,21 @@
 from django.shortcuts import render
 #from django.http import HttpResponse
 from .models import Post, Tag
-
+from django.views.generic import View
 def hello(request):
     p= Post.objects.all()
     return render(request, 'blog/index.html', context={'posts': p})
 
-def post_detail(request, slug):
-    p = Post.objects.get(slug__iexact=slug)
-    return render(request, 'blog/post_detail.html',context={'post': p})
 
+class PostDetail(View):
+    def get(self, request, slug):
+        p = Post.objects.get(slug__iexact=slug)
+        return render(request, 'blog/post_detail.html', context={'post': p})
 def tags_list(request):
     tags = Tag.objects.all()
     return render(request, 'blog/tags_list.html', context ={'tags':tags})
-def tag_detail(request, slug):
-    tag = Tag.objects.get(slug__iexact=slug)
-    return render(request,'blog/tag_detail.html', context={'tag':tag})
+
+class TagDetail(View):
+    def get(self, request,slug):
+        tag = Tag.objects.get(slug__iexact=slug)
+        return render(request, 'blog/tag_detail.html', context={'tag': tag})
