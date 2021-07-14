ea97c4e84cc37b90713752759ef5ccc1b0a7028c
Ihor Kolisnyk <ihorko95@gmail.com>
2021-07-14 12:35:53 +0300

    tegs refacroring

--- blog/utils.py
+++ blog/utils.py
@@ -14,26 +14,40 @@ class ObjectDetailMixin:
 class ObjectCreateMixin:
     form_model = None
     template = None
+
     def get(self, request):
         form = self.form_model
         return render(request, self.template, context={'form': form})
 
-    def post(self,request):
+    def post(self, request):
         bound_form = self.form_model(request.POST)
         if bound_form.is_valid():
-            new_tag = bound_form.save()
-            return  redirect(new_tag)
+            new_obj = bound_form.save()
+            return redirect(new_obj)
         return render(request, self.template, context={'form': bound_form})
 
 class ObjectUpdateMixin:
     model = None
     form_model = None
     template = None
+
     def get(self,request,slug):
         obj = self.model.objects.get(slug__iexact=slug)
         bound_form = self.form_model(instance=obj)
         return render(request, self.template, context={
             'form': bound_form,
             #'tag': obj
-            str(self.model).lower(): obj
+            self.model.__name__.lower(): obj
+        })
+
+    def post(self, request, slug):
+        obj = self.model.objects.get(slug__iexact=slug)
+        bound_form = self.form_model(request.POST, instance=obj)
+        if bound_form.is_valid():
+            new_obj = bound_form.save()
+            print(new_obj)
+            return redirect(new_obj)
+        return render(request, self.template, context={
+            'form': bound_form,
+            self.model.__name__.lower(): obj
         })
